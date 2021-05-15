ed026e9e7e2c8f9661cac9b28f86defa049506a4
paulius <p.matulionis@gmail.com>
2021-05-15 16:42:24 +0300

    Written java docs for the main service and dao interfaces.

--- src/main/java/co/uk/realisticgames/task/dao/CarDao.java
+++ src/main/java/co/uk/realisticgames/task/dao/CarDao.java
@@ -13,11 +13,38 @@ import java.util.List;
  */
 public interface CarDao {
 
+    /**
+     * Finds the data by the given filter parameters and sorts by the given sort parameters. If neither of them is
+     * provided then all the data is returned. If filter parameters are provided the data is filtered matching exact
+     * values.
+     *
+     * @param sortParams   sort parameters
+     * @param filterParams filter parameters
+     * @return a list of car data
+     */
     List<CarDataDto> findData(SortParams sortParams, FilterParams filterParams);
 
+    /**
+     * Updates the given car data by the given ID. As there may be duplicates by the same ID, all records that are
+     * matched will be updated.
+     *
+     * @param id  id to find the records to update
+     * @param dto car data object
+     */
     void update(Integer id, CarDataDto dto);
 
+    /**
+     * Creates completely new car data entry in the existing data list.
+     *
+     * @param dto car data object
+     */
     void create(CarDataDto dto);
 
+    /**
+     * Deletes car data record(s) that is/are matched by the given ID. As there may be multiple records found by
+     * the given ID, they all be deleted.
+     *
+     * @param id the ID of the car data
+     */
     void delete(Integer id);
 }
--- src/main/java/co/uk/realisticgames/task/service/CarService.java
+++ src/main/java/co/uk/realisticgames/task/service/CarService.java
@@ -14,11 +14,42 @@ import java.util.List;
  */
 public interface CarService {
 
+    /**
+     * A service method which maps the data from the API model to DTO (data transfer) object when the data is being
+     * filtered This operation might do some other manipulation with the data as well. For e.g. it could filter out the
+     * records that do not have certain properties as longs as all the retrieval and data filtering happens in the data
+     * storage level.
+     *
+     * @param sortParams   sort parameters
+     * @param filterParams filter parameters
+     * @return a list of car data objects
+     */
     List<CarDataItem> findData(SortParams sortParams, FilterParams filterParams);
 
+    /**
+     * A service method which maps the data from the API model to DTO (data transfer) object when the data is being
+     * updated. This method can also do other manipulation with the data before it is actually updated.
+     *
+     * @param id   id to find car data
+     * @param item API model property which holds car data details
+     * @return response object containing a response message
+     */
     ResponseItem update(Integer id, CarDataItem item);
 
+    /**
+     * A service method which maps the data from the API model to DTO (data transfer) object when the data is being
+     * created. This method can also do other manipulation with the data before it is actually updated.
+     *
+     * @param item API model property which holds car data details
+     * @return response object containing a response message
+     */
     ResponseItem create(CarDataItem item);
 
+    /**
+     * A service method which calls data storage API to actually remove the data from storage.
+     *
+     * @param id ID of the data that need to be deleted
+     * @return response object containing a response message
+     */
     ResponseItem remove(Integer id);
 }
